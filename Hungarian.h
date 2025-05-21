e12566700d68210fb1344262d05e8a30a8bdd417
Samiran Kawtikwar <samkawtikwar@gmail.com>
2025-05-21 11:24:02 -0500

    make assignmentoptimal public

--- Hungarian.h
+++ Hungarian.h
@@ -1,13 +1,13 @@
 ///////////////////////////////////////////////////////////////////////////////
 // Hungarian.h: Header file for Class HungarianAlgorithm.
-// 
+//
 // This is a C++ wrapper with slight modification of a hungarian algorithm implementation by Markus Buehren.
 // The original implementation is a few mex-functions for use in MATLAB, found here:
 // http://www.mathworks.com/matlabcentral/fileexchange/6543-functions-for-the-rectangular-assignment-problem
-// 
+//
 // Both this code and the orignal code are published under the BSD license.
 // by Cong Ma, 2016
-// 
+//
 
 #ifndef HUNGARIAN_H
 #define HUNGARIAN_H
@@ -17,16 +17,15 @@
 
 using namespace std;
 
-
 class HungarianAlgorithm
 {
 public:
 	HungarianAlgorithm();
 	~HungarianAlgorithm();
-	double Solve(vector <vector<double> >& DistMatrix, vector<int>& Assignment);
+	double Solve(vector<vector<double>> &DistMatrix, vector<int> &Assignment);
+	void assignmentoptimal(int *assignment, double *cost, double *distMatrix, int nOfRows, int nOfColumns);
 
 private:
-	void assignmentoptimal(int *assignment, double *cost, double *distMatrix, int nOfRows, int nOfColumns);
 	void buildassignmentvector(int *assignment, bool *starMatrix, int nOfRows, int nOfColumns);
 	void computeassignmentcost(int *assignment, double *cost, double *distMatrix, int nOfRows);
 	void step2a(int *assignment, double *distMatrix, bool *starMatrix, bool *newStarMatrix, bool *primeMatrix, bool *coveredColumns, bool *coveredRows, int nOfRows, int nOfColumns, int minDim);
@@ -35,6 +34,5 @@ private:
 	void step4(int *assignment, double *distMatrix, bool *starMatrix, bool *newStarMatrix, bool *primeMatrix, bool *coveredColumns, bool *coveredRows, int nOfRows, int nOfColumns, int minDim, int row, int col);
 	void step5(int *assignment, double *distMatrix, bool *starMatrix, bool *newStarMatrix, bool *primeMatrix, bool *coveredColumns, bool *coveredRows, int nOfRows, int nOfColumns, int minDim);
 };
-
 
 #endif
